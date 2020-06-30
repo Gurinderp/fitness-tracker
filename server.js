8b0449952ed408fe538b41179e7c418ed9ac7e2c
rednirug <sgurinderpreet@gmail.com>
2020-06-30 02:26:11 -0700

    models and routes

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const app = express();
+
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
+
+const PORT = 3000;
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});
